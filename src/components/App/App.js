f632e9a06b25ec9ae85fcaabc22a411da373956c
Judy Ye <judyy.ye@gmail.com>
2023-10-30 13:56:15 -0700

    Add successful GET request to App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,21 @@
+import { useEffect } from 'react';
 import './App.css';
 import Header from './Header/Header';
+import { fetchTopScienceHeadlines } from '../../apiCalls';
 
 function App() {
+  useEffect(() => {
+    const getTopSceienceHeadlines = async () => {
+      try {
+        const topScienceHeadlines = await fetchTopScienceHeadlines()
+        console.log(topScienceHeadlines.articles)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getTopSceienceHeadlines() 
+  }, [])
+
   return (
     <div>
       <Header />
