525d8bd015f26bf55a99782127db0e2e386b7eb2
Judy Ye <judyy.ye@gmail.com>
2023-10-30 22:42:38 -0700

    Add error handling to App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,11 +6,13 @@ import Header from '../Header/Header';
 import Articles from '../Articles/Articles';
 import DetailedArticle from '../DetailedArticle/DetailedArticle';
 import Search from '../Search/Search';
+import Error from '../Error/Error';
 import { Route, Routes, useLocation } from 'react-router-dom';
 
 function App() {
   const [articles, setArticles] = useState([])
   const [search, setSearch] = useState('')
+  const [error, setError] = useState('')
   const location = useLocation()
  
   useEffect(() => {
@@ -22,9 +24,11 @@ function App() {
       try {
         // const topScienceHeadlines = await fetchTopScienceHeadlines()
         // console.log(topScienceHeadlines.articles)
+        // setArticles(topScienceHeadlines.articles)
         setArticles(mockData.articles)
-      } catch (error) {
-        console.log(error)
+      } catch (err) {
+        console.log(err)
+        setError(`${err.message}: Something went wrong`)
       }
     }
     getTopSceienceHeadlines() 
@@ -33,10 +37,12 @@ function App() {
   return (
     <div>
       <Header />
-      <Routes>
+     {error ? <Error error={error}/> :  
+     <Routes>
         <Route path='/' element={<><Search search={search} setSearch={setSearch}/> <Articles articles={articles} search={search}/>  </> }/>
         <Route path='/details/:articleTitle' element={<DetailedArticle articles={articles}/>}/>
-      </Routes>
+        <Route path='*' element={<Error error={error}/>}/>
+      </Routes>}
     </div>
   );
 }
