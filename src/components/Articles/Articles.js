0dc748178c0526e86e1084057961ca4e1c3451ae
Judy Ye <judyy.ye@gmail.com>
2023-11-01 10:38:59 -0700

    Add message to notify user of no matching results

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -2,39 +2,43 @@ import { Link } from 'react-router-dom';
 import './Articles.css';
 
 export default function Articles({ articles, search }) {
-  return (
-    articles
-      .filter((article) => {
-        return Object.values(article).some((value) => {
-          if (typeof value === 'string') {
-            return value.toLowerCase().includes(search.toLowerCase());
-          }
-        });
-      })
-      .map((article, index) => {
-        const reducedArticleDate = article.publishedAt.substring(0, 10);
-        const articleDate = new Date(reducedArticleDate);
-        const formattedDate = `${
-          articleDate.getMonth() + 1
-        }/${articleDate.getUTCDate()}/${articleDate.getFullYear()}`;
-        return (
-          <section className="articles" key={index}>
-            <div>
-              <img
-                className="main-display-images"
-                src={article.urlToImage}
-                alt={article.title}></img>
-            </div>
-            <div>
-              <h2>{article.title}</h2>
-              <p>{article.description}</p>
-              <p>{formattedDate}</p>
-              <Link className='see-more' to={`/details/${encodeURIComponent(article.title)}`}>
-                See More
-              </Link>
-            </div>
-          </section>
-        );
-      })
-  );
+  const searchedArticles = articles.filter((article) => {
+    return Object.values(article).some((value) => {
+      if (typeof value === 'string') {
+        return value.toLowerCase().includes(search.toLowerCase());
+      }
+    });
+  });
+
+  if (searchedArticles.length === 0) {
+    return <p className='no-results'>Sorry, no articles match your search. Please try something else!</p>;
+  }
+
+  return searchedArticles.map((article, index) => {
+    const reducedArticleDate = article.publishedAt.substring(0, 10);
+    const articleDate = new Date(reducedArticleDate);
+    const formattedDate = `${
+      articleDate.getMonth() + 1
+    }/${articleDate.getUTCDate()}/${articleDate.getFullYear()}`;
+    return (
+      <section className="articles" key={index}>
+        <div>
+          <img
+            className="main-display-images"
+            src={article.urlToImage}
+            alt={article.title}></img>
+        </div>
+        <div>
+          <h2>{article.title}</h2>
+          <p>{article.description}</p>
+          <p>{formattedDate}</p>
+          <Link
+            className="see-more"
+            to={`/details/${encodeURIComponent(article.title)}`}>
+            See More
+          </Link>
+        </div>
+      </section>
+    );
+  });
 }
